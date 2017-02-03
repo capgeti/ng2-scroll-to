9c707b3e3794f2748ae8675aa035c82698c1e494
pavka <d.pavka@gmail.com>
2017-02-03 10:59:43 +0200

    wrapped in ngModule

--- ng2-scroll-to.ts
+++ ng2-scroll-to.ts
@@ -1,1 +1,10 @@
-export {ScrollTo} from './src/scrollTo';
+import {NgModule}      from '@angular/core';
+import {ScrollTo} from "./src/scrollTo";
+//export {ScrollTo} from './src/scrollTo';
+
+@NgModule({
+    declarations: [ScrollTo],
+    exports: [ScrollTo],
+})
+export class ScrollToModule {
+}
